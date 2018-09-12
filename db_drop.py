e297a6f337079253d73d1ee65b392275ef5ae407
Viacheslav Haidei <haideigv@gmail.com>
2018-09-13 02:05:14 +0300

    added aggregator, validator

--- db_drop.py
+++ db_drop.py
@@ -3,17 +3,9 @@ import psycopg2
 conn = psycopg2.connect("dbname=postgres user=postgres")
 cursor = conn.cursor()
 
-cursor.execute(
-    "DROP TABLE raw_data"
-)
-
-cursor.execute(
-    "DROP TABLE agg_data;"
-)
-
-cursor.execute(
-    "DROP TABLE last_processed_id;"
-)
+cursor.execute("DROP TABLE raw_data")
+cursor.execute("DROP TABLE agg_data;")
+cursor.execute("DROP TABLE last_processed_id;")
 
 conn.commit()
 
